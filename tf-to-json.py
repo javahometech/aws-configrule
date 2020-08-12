b4d08287be50d7f312b4d824fb27a68a5194533f
Praveen Kumar Kanna <pkumark6@cisco.com>
2020-08-12 13:00:19 -0400

    updated script with arguments

--- tf-to-json.py
+++ tf-to-json.py
@@ -1,8 +1,32 @@
+#!/usr/bin/env python3
+
 import hcl
 import json
+import argparse
+"""
+Usage : python3 tf-to-json.py --input configrule.tf --output config-out.json 
 
-with open('configrule.tf', 'r') as fp:
-    json_out = obj = hcl.load(fp)
+"""
 
-with open('config-out.json', 'w') as outfile:
-    json.dump(json_out, outfile, indent=4)
+
+def tf_to_json(input_file, output_file):
+
+    with open('configrule.tf', 'r') as fp:
+        json_out = hcl.load(fp)
+
+    with open('config-out.json', 'w') as outfile:
+        json.dump(json_out, outfile, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='converting tf file to json file')
+    parser.add_argument('--input',
+                        help='give the input tf file to change',
+                        required=True)
+    parser.add_argument('--output',
+                        help='give the output json file to create',
+                        required=True)
+    args = parser.parse_args()
+
+    tf_to_json(args.input, args.output)
